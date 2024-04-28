5e73590f813d5c4bbad07b98309ab4109fcc506e
reyzacaloh <ignite@IGNITE>
2024-04-28 14:15:52 +0700

    add mesh topo to script

--- run_hpcc_pint256_fb.sh
+++ run_hpcc_pint256_fb.sh
@@ -1,2 +1,2 @@
 cd simulation/
-python run.py --cc hpccPint --trace fb50_b100 --bw 100 --hpai 50 --utgt 95 --pint_log_base 1.05 --pint_prob 0.00390625
+python run.py --cc hpccPint --topo mesh_topology --trace fb50_b100 --bw 100 --hpai 50 --utgt 95 --pint_log_base 1.05 --pint_prob 0.00390625
